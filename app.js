7ca8ccc3bb9900a638c6bd7ea2aad5c2d4839e10
bhatiaAnanya <47259369+bhatiaAnanya@users.noreply.github.com>
2020-06-14 03:13:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = 5000;
+const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
